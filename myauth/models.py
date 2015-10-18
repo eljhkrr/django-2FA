cc062092c9d419c3e63ae3472e59c1415c7ac522
EL <eljhkrr@gmail.com>
2015-10-18 20:00:10 +0300

    simplified my auth model

--- myauth/models.py
+++ myauth/models.py
@@ -3,8 +3,5 @@ from django.contrib.auth.models import User
 
 class Two_factor(models.Model):
 	user = models.OneToOneField(User)
-	phone_number = models.CharField(max_length=20)
-	phone_token = models.CharField(max_length=6)
-	phone_verified = models.BooleanField()
 	email_token = models.CharField(max_length=100)
 	email_verified = models.BooleanField()
