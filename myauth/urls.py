5de0ccfd1c5a3f4d9adf4d89edc974f35089212c
EL <eljhkrr@gmail.com>
2015-10-18 20:46:00 +0300

    Removed unnecessary urls

--- myauth/urls.py
+++ myauth/urls.py
@@ -3,14 +3,9 @@ from django.conf.urls import url
 from myauth import views
 
 urlpatterns = [
-	url(r'^signin/$', views.signin, name='signin'),
 	url(r'^register/$', views.register, name='register'),
-	url(r'^verify_user/$', views.verify_user, name='verify_user'),
-	url(r'sms/$', views.sms_verify, name="verify_sms"),
-	url(r'checktoken/$', views.check_token, name="check_token"),
 	url(r'registersubmit/$', views.register_submit, name="register_submit"),
 	url(r'confirmemail/$', views.confirm_email, name="confirm_email"),
-	url(r'content/$', views.content, name="content"),
 	url(r'restricted/$', views.restricted, name="restricted"),
 	url(r'logout/$', views.logoutview, name="logout"),
 ]
