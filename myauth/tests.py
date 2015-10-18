2704e937a4df70083d6a2d9ded049a4a5b902d8f
EL <eljhkrr@gmail.com>
2015-10-19 02:32:33 +0300

    Removed stale tests

--- myauth/tests.py
+++ myauth/tests.py
@@ -5,17 +5,10 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 
 class ViewTests(TestCase):
-	def test_signin(self):
-		response = self.client.get(reverse('myauth:signin'))
-		self.assertEqual(response.status_code, 200)
-
 	def test_register(self):
 		response = self.client.get(reverse('myauth:register'))
 		self.assertEqual(response.status_code, 200)
 
-	def test_non_signed_in_user(self):
-		response = self.client.get(reverse('myauth:content'))
-		self.assertEqual(response.status_code, 302)
 
 class TwilioTest(TestCase):
 	def test_twilio_receive(self):
