5af4e83f6fc614afc70c72b76a5a8b9e75c9294e
EL <eljhkrr@gmail.com>
2015-10-30 14:48:59 +0300

    cleanup

--- myauth/signals.py
+++ myauth/signals.py
@@ -6,5 +6,4 @@ from myauth.views import send_confirmation_mail
 @receiver(post_save, sender=Two_factor)
 def tf_post_save(sender, **kwargs):
 	instance = kwargs['instance']
-	print "User email: ", instance.user.email
 	send_confirmation_mail(instance.user.email, instance.email_token, "Baaaaaah!!")
